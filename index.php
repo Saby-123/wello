82a5c7f84b46e0e39eb84ec324244ed4541d5e2b
Saby-123 <84474992+Saby-123@users.noreply.github.com>
2021-12-24 10:00:38 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$vist_page     =   "index.php";
+include "logger.php";
+?>
+
+<html> 
+<head> 
+<title> Merry Christmas 2021 </title> 
+</head>
+<body>
+<p> Lmao </p>
+<p> Your ip just got logged :p </p> 
+</body>
+</html> 
